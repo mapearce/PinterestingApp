489da2d99788ea72c3831d1897bb1b0289157c65
Michael Pearce <themikepearce@gmail.com>
2015-06-06 12:22:43 -0700

    Many changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def home
   end
+  def wood
+  end
+  def paper
+  end
+  def metal
+  end
+  def oneszeroes
+  end
+  def plastic
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Portfolio::Application.routes.draw do
-  get "pages/home"
+  root 'pages#home'
+  get 'wood' => 'pages#wood'
+  get 'paper' => 'pages#paper'
+  get 'metal' => 'pages#metal'
+  get 'oneszeroes' => 'pages#oneszeroes'
+  get 'plastic' => 'pages#plastic'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
